4b5839fcf13c99b4d3484f673b1ebffdbfee569d
Aziz010000 <juventus201510@gmai.com>
2024-08-23 16:36:53 +0200

    mod

--- minHeap.py
+++ minHeap.py
@@ -21,18 +21,21 @@ class minHeap:
 		self.__ordinateUp(self.size-1)
 	
 	def __ordinateDown(self,i):
-		if i+1<self.size and self.L[i] > self.L[i+1]: 
-			self.__swap(i, i+1)
-			self.__ordinateDown(i+1)
-		elif +2<self.size and self.L[i] > self.L[i+2]:
-			self.__swap(i, i+2)
-			self.__ordinateDown(i+2)
+		index = i
+		if 2*i+1 < self.size and self.L[index] > self.L[i*2+1]: 
+			index = i*2+1
+		if 2*i+2 < self.size and self.L[index] > self.L[i*2+2]: 
+			index = i*2+2
+		if index != i :
+			self.__swap(i, index) 
+			self.__ordinateDown(index)
 	
 	def dequeque(self) : 
 		if(self.size > 0): 
 			elem = self.L[0]
-			self.L.pop(0)
 			self.size = self.size -1
+			self.L[0] = self.L[len(self.L)-1]
+			self.L.pop(len(self.L)-1)
 			self.__ordinateDown(0)
 			return elem
 		return -1
@@ -40,3 +43,29 @@ class minHeap:
 	def visualize(self): 
 		print(self.L)
 
+
+myHeap = minHeap() 
+myHeap.enqueque(27)
+myHeap.enqueque(5)
+myHeap.dequeque()
+myHeap.enqueque(2)
+myHeap.enqueque(4)
+myHeap.enqueque(1)
+myHeap.enqueque(28)
+myHeap.dequeque()
+myHeap.enqueque(9)
+myHeap.enqueque(6)
+myHeap.enqueque(11)
+myHeap.enqueque(77)
+myHeap.enqueque(14)
+myHeap.enqueque(10)
+myHeap.enqueque(21)
+myHeap.enqueque(108)
+myHeap.visualize()
+myHeap.dequeque()
+myHeap.dequeque()
+myHeap.dequeque()
+myHeap.dequeque()
+myHeap.dequeque()
+print("VISUALIZZANDO")
+myHeap.visualize()
